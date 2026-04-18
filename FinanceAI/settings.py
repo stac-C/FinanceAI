d2cd94511c054b54e9b2b531c5a09f8b31dfb355
ClaudyLumen <claudysta@gmail.com>
2026-04-18 17:38:36 +0100

    update x

--- FinanceAI/settings.py
+++ FinanceAI/settings.py
@@ -119,7 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "prediction" / "static",
+]
 
+static_root = BASE_DIR / "staticfiles"
+STATIC_ROOT = static_root
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
